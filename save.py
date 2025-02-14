8ca35466520b71dac95051d32bd489eec0bbe78a
Samy Belazzoug <samy.belazzoug@laplateforme.io>
2025-02-14 11:15:43 +0100

    Finished Save Class,(we can do CRUD with JSON) + modified base file name

--- save.py
+++ save.py
@@ -4,28 +4,65 @@ class Save():
     def __init__(self,name:str="Save1",pokedex:list=[]):
         self.name = name
         self.pokedex = pokedex
-        self.data = """{"Name":self.name,
-                     "Pokedex":self.pokedex
-                     }"""
-    
-    # def create(self):
-    #     """Create a backup and save it to JSON file"""
-    #     save = json.dump({"Name":self.name,"Pokedex":self.pokedex})
-    #     with open('pokedex.json','w') as f:
-    #         pass
-    
+        self.js = { "Name": self.name, "Pokedex":self.pokedex}
             
     def read(self):
-        print(type(self.data))
-        data = json.loads(self.data)
-        print(data)
+        with open('pokedex.json','r') as f:
+            read = json.load(f)
+            print(read)
 
+    def update(self):
+        with open('pokedex.json','w') as f:
+            json.dump(self.js,f)
 
-    def update():
-        pass
+    def modify_name(self,value):
+        for i in self.js:
+            if i == 'Name':
+                self.js[i] = value
+        self.update()
 
-    def delete():
-        pass
+    def modify_list(self,option:int,pokemon:str,replace:str):
+        for i in self.pokedex:
+            if option == 0:
+                print("you are useless but getting complexity worse.")
+                pass
+            if option == 1:
+                self.pokedex.append(pokemon)
+                self.update()
+                break
+            if option == 3:
+                    if i == pokemon:
+                        self.pokedex.remove(pokemon)
+                        self.update()
+                        break
+        if option == 2:
+            for i in range(len(self.pokedex)):
+                    if self.pokedex[i] == pokemon:
+                        self.pokedex[i] = replace
+                        self.js['Pokedex'] = self.pokedex
+                        self.update()
+                        break
 
-sauvegarde = Save("OK",[])
-sauvegarde.read()
+    def delete(self):
+        self.js['Name'] = "Par default"
+        self.js['Pokedex'] = []
+        self.update()
+
+if __name__ == "__main__":
+    sauvegarde = Save("OK",["Salameche","Pikachu"])
+    sauvegarde.update()
+    sauvegarde.read()
+    sauvegarde.modify_name("YO G SON")
+    sauvegarde.read()
+    sauvegarde.modify_name("J Is Son")
+    sauvegarde.read()
+    sauvegarde.modify_list(1,"Bulbizzare",None)
+    sauvegarde.read()
+    sauvegarde.modify_list(2,'Bulbizzare', 'Florizzare')
+    sauvegarde.read()
+    sauvegarde.modify_list(3,"Salameche",None)
+    sauvegarde.read()
+    sauvegarde.modify_name("Nice :)")
+    sauvegarde.read()
+    sauvegarde.delete()
+    sauvegarde.read()
